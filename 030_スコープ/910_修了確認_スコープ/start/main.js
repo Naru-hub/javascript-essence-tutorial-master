ab1a8a0da3d3f3a6a2f55ff0207d717de28c102a
Naru-hub <1208ny0324@gmail.com>
2022-08-02 22:31:36 +0900

    終了確認 スコープ

--- 030_スコープ/910_修了確認_スコープ/start/main.js
+++ 030_スコープ/910_修了確認_スコープ/start/main.js
@@ -3,14 +3,15 @@
  * 以下のコードではエラーが発生します。
  * コンソールで"fn called"と表示されるように
  * fn内のコードを変更してください。
- * 
+ *
  * ※if文は削除してはいけません。
  */
 function fn() {
+    let a;
     if(true) {
-        let a = 'fn called';
+        a = 'fn called';
     }
-    return a; // ReferenceError: a is not defined
+    return a;
 }
 
 const result = fn();
@@ -26,7 +27,7 @@ function fn2() {
     console.log(val); // 期待値->'val1'
 
     if(true) {
-        var val = 'val2';
+        let val = 'val2';
         console.log(val); // 期待値->'val2'
     }
 
@@ -39,10 +40,24 @@ fn2();
  * 「クロージャー（プライベート変数）」のレクチャーで作成
  * したincrementと同じ機能を持つincrement関数をブロック
  * スコープとクロージャーを利用して作成してみてください。
- * 
+ *
  * increment(); // 期待値->1
  * increment(); // 期待値->2
  * increment(); // 期待値->3
  * increment(); // 期待値->4
  */
 
+{
+	let num = 0;
+	function increment() {
+			num =  num + 1;
+			console.log(num);
+	}
+}
+
+// letはブロックスコープ内からしか参照できない
+// functionやvarではブロックスコープは無視されるのでincrement()はブロックスコープの外からでも実行できる
+increment();
+increment();
+increment();
+increment();
