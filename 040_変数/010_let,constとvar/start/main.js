72f74c17d0426b3227b56fabaacfb32d0f2e6bdd
Naru-hub <1208ny0324@gmail.com>
2022-08-04 23:28:59 +0900

    let, constとvar

---
+++ 040_変数/010_let,constとvar/start/main.js
@@ -0,0 +1,25 @@
+// 再宣言について
+// let a = 0;
+// let a = 0;
+
+// var b = 0;
+// var b = 1;
+
+// 再代入について
+// let c = 0;
+// c = 1;
+
+// const d = 0;
+// d = 1;
+
+// ブロックスコープ
+{
+  let e = 0;
+  var f = 0;
+}
+
+// ホイスティング
+console.log(h);
+let g = 0;
+var h = 0;   //undefined
+
